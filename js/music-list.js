fa8123074dbedf6274741ce260df2ee5aef6255c
Gautam Rana <gsmartgautam@gmail.com>
2022-07-23 14:30:48 +0530

    23/7/2022

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Oh_Isha_-_Major",
+        artist: "Adivi_Sesh",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Mehabooba_Video_Song_Hindi KGF",
+        artist: "Mehabooba Song",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Tumse_Bhi_Zyada Tadap",
+        artist: "Arman Malik Tadap",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Tujhe_Kitna_Chahne_Lage",
+        artist: "Argit Singh",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Tere_Sang_Yaara_-_Full_Audio",
+        artist: "Argit Singh",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Aashiqui_Aa_Gayi Radhe Shyam",
+        artist: "Arman Malik",
+        img: "music-6",
+        src: "music-6"
+    }
+]
